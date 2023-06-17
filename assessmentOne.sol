9a0d2a43b7736fe1216a216b51e3c3bfb22ae6fd
Hemin Patel <70432088+iMPERIAL18@users.noreply.github.com>
2023-06-17 10:39:45 +0530

    Update assessmentOne.sol

--- assessmentOne.sol
+++ assessmentOne.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 contract AssessmentOne {
     mapping(address => uint) balances;
 
-    function depositCoin(address _address, uint _value) public {
+    function depositCoin(address _address, uint _value) public payable {
         require(_value >= 500, "Minimum amount required to deposit is 500");
         balances[_address] += _value;
     }
